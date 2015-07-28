238d16b567f3891966f0d75d9109e1c3f8812263
Dwayne Jeng <dwayne.jeng@sagebase.org>
2015-07-28 16:45:48 -0700

    convert to worker app

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,20 +1,22 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application implements CommandLineRunner {
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
     @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
+    public void run(String... strings) {
+        System.out.println("Hello world!");
+        LOG.info("Hello logs!");
     }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
